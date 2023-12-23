f2253971c65c034d6baf26b3de5ccdd975b6c537
Said0ra <szytaner@gmail.com>
2023-12-23 19:02:43 +0100

    completed Lab3

--- Lab3/main.py
+++ Lab3/main.py
@@ -1,4 +1,5 @@
 import random
+import string
 
 if __name__ == '__main__':
     task = int(input("Zadanie: "))
@@ -57,31 +58,110 @@ if __name__ == '__main__':
 
         print(f"f) {b}")
     elif task == 4:
-        phrase = input("Wpisz zdanie: ")
-        sortedPhrase = sorted(phrase)
-        print(sortedPhrase)
+        ciag = input("Podaj ciąg znaków: ")
+        unikalne_znaki = set()
+        zmieniony_ciag = ""
+
+        for znak in ciag:
+            if znak in unikalne_znaki:
+                zmieniony_ciag += "@"
+            else:
+                zmieniony_ciag += znak
+                unikalne_znaki.add(znak)
+
+        print(f"Wynik zmiany: {zmieniony_ciag}")
+    elif task == 5:
+        danie = input("Podaj zdanie: ")
+        slowa = zdanie.split()
+
+        najdluzsze_slowo = max(slowa, key=len)
+        dlugosc_najdluzszego = len(najdluzsze_slowo)
+
+        print("Najdłuższe słowo:", najdluzsze_slowo)
+        print("Długość najdłuższego słowa:", dlugosc_najdluzszego)
+    elif task == 6:
+        ciag = input("Podaj ciąg znaków: ")
+        if ciag[::-1] == ciag:
+            print("Ciag znaków jest palindromem.")
+        else:
+            print("Ciąg znaków nie jest palindromem.")
+    elif task == 7:
+        n = int(input("Podaj n: "))
+        x = int(input("Podaj x: "))
+
+        lista = ["".join(random.choices(string.ascii_lowercase, k=x)) for _ in range(n)]
+
+        ilosc_znakow = sum(len(element) for element in lista)
+        print("a) Ilość znaków w liście:", ilosc_znakow)
+
+        ilosc_k = sum(element.count('k') for element in lista)
+        print("b) Ilość liter 'k' w liście:", ilosc_k)
+
+        ilosc_kt = sum(element.count('kt') for element in lista)
+        print("c) Ilość ciągów 'kt' w liście:", ilosc_kt)
+
+        s = int(input("Podaj s: "))
+        ilosc_dluzszych_niz_s = sum(1 for element in lista if len(element) > s)
+        print(f"d) Ilość ciągów dłuższych niż {s}: {ilosc_dluzszych_niz_s}")
+
+        lista_zmieniona = ['a' + element + 'z' for element in lista]
+        print("e) Zmieniona lista:", lista_zmieniona)
+    elif task == 8:
+        alfabet = list(string.ascii_lowercase)
+        n = int(input("Podaj n: "))
+
+        podlisty = [alfabet[i:i + n] for i in range(0, len(alfabet), n)]
+        print("Podzielona lista:", podlisty)
     elif task == 10:  # zestaw nr. 2
-        n = int(input("Ile elementów ma mieć lista? "))
-        x = int(input("Maksymalna liczba znaków w łańcuchu: "))
+        n = int(input("Podaj n: "))
+        x = int(input("Podaj x: "))
+        lista = [''.join(random.choices(string.ascii_lowercase, k=x)) for _ in range(n)]
 
-        words = []
+        krotka = tuple(lista)
 
-        for i in range(n):
+        ilosc_znakow_krotka = sum(len(element) for element in krotka)
+        print("a) Ilość znaków w krotce:", ilosc_znakow_krotka)
 
-            word = ""
-            end = 0
+        ilosc_k_krotka = sum(element.count('k') for element in krotka)
+        print("b) Ilość liter 'k' w krotce:", ilosc_k_krotka)
 
-            for k in range(x):
-                if end >= 0.8:
-                    break
-                else:
-                    word += chr(random.randint(65, 90))
+        ilosc_kt_krotka = sum(element.count('kt') for element in krotka)
+        print("c) Ilość ciągów 'kt' w krotce:", ilosc_kt_krotka)
 
-                end = random.random()
+        s = int(input("Podaj s: "))
+        ilosc_dluzszych_niz_s_krotka = sum(1 for element in krotka if len(element) > s)
+        print(f"d) Ilość ciągów dłuższych niż {s} w krotce: {ilosc_dluzszych_niz_s_krotka}")
+    elif task == 11:
+        zakupy = {'jajka': 3.5, 'chleb': 2.0, 'mleko': 2.5, 'ser': 5.0}
 
-            words.append(word)
+        print("Lista zakupów:")
+        for produkt, cena in zakupy.items():
+            print(f"{produkt}: {cena} PLN")
 
-        print(words)
+        suma_wydatkow = sum(zakupy.values())
+        print(f"Suma wydatków: {suma_wydatkow} PLN")
+    elif task == 12:
+        rachunki_prad = {'styczeń': 200, 'luty': 180, 'marzec': 220, 'kwiecień': 190, 'maj': 210}
 
-        krotka = tuple(words)
-        print(krotka)
+        wartosci = list(rachunki_prad.values())
+        maksimum = max(wartosci)
+        minimum = min(wartosci)
+        suma = sum(wartosci)
+        srednia = suma / len(wartosci)
+
+        print("Maksymalna wartość:", maksimum)
+        print("Minimalna wartość:", minimum)
+        print("Suma wartości:", suma)
+        print("Średnia wartość:", srednia)
+
+        ostatni_miesiac = list(rachunki_prad.keys())[-1]
+        ostatni_rachunek = rachunki_prad[ostatni_miesiac]
+
+        if ostatni_rachunek > srednia:
+            print(f"Wartość rachunku za {ostatni_miesiac} przekroczyła średnią.")
+        else:
+            print(f"Wartość rachunku za {ostatni_miesiac} nie przekroczyła średniej.")
+def generate_random_string(length):
+    import random
+    import string
+    return ''.join(random.choice(string.ascii_lowercase) for _ in range(length))
